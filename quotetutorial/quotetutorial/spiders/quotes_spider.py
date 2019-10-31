c2681ea28a2fdaa90f259690bd8bcf88a7cea799
rahi25 <ehsan.rahnama25@gmail.com>
2019-10-31 23:07:22 +0330

    Used loging form

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,13 +1,35 @@
 import scrapy
-
+from ..items import QuotetutorialItem
+from scrapy.http import FormRequest
+from scrapy.utils.response import open_in_browser
 
 class QuoteSpider(scrapy.Spider):
     # define variable and it is important to write them exactly
     name = 'quotes'
     start_urls = [
-        'http://quotes.toscrape.com/'
+        'http://quotes.toscrape.com/login'
     ]
 
     def parse(self, response):
-        title = response.css('title::text').extract()
-        yield {'titletext': title}
+        token = response.css('form input::attr(value)').extract_first()
+        return FormRequest.from_response(response, formdata={
+            'csrf_token': token,
+            'username': 'Ehsanrah',
+            'password': 'egdsfsadas'
+        }, callback=self.start_scraping)
+
+    def start_scraping(self, response):
+        open_in_browser(response)
+        items = QuotetutorialItem()
+
+        all_div_quotes = response.css('div.quote')
+        for quotes in all_div_quotes:
+            quote = quotes.css("span.text::text").extract()
+            author = quotes.css(".author::text").extract()
+            tag = quotes.css(".tag::text").extract()
+
+            items['quote'] = quote
+            items['author'] = author
+            items['tag'] = tag
+
+            yield items
